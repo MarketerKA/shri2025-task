ec40d29a6b10948d8bd00b297ee030c0918683f0
MarketerKA <karhipov606@gmail.com>
2025-07-06 23:32:23 +0300

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
-import './styles.css';
-import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <>
